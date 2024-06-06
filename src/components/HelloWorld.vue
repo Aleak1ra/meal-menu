48a34b03a4e098ae6e4ee659050ff5bd17074132
Alexandre <alexandre_bernardi95@hotmail.com>
2024-06-05 22:59:28 -0300

    chore: first commit cfg setup

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+
+</script>
+
+<template>
+  <h1 class="bg-green-200 bg-opacity-50">{{ msg }}</h1>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
