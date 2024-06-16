05460510e68033dc254ea5eaa897ffab2cf97bf8
Alexandre <alexandre_bernardi95@hotmail.com>
2024-06-16 20:49:12 -0300

    feat: implementado barra de pesquisa

--- views/Home.vue
+++ views/Home.vue
@@ -1,22 +1,72 @@
 <template>
   <VCard class="pa-5 d-flex justify-center flex-column" width="1200px" variant="outlined">
     <VTextField
-      label="Search meals"
+      v-model="search"
+      label="Pesquisar um prato"
       type="text"
       variant="outlined"
       clearable
     ></VTextField>
     <div class="d-flex align-center justify-center">
-      <router-link class="d-flex justify-center px-1" :to="{name: 'ByLetter', params: {letter}}" v-for="letter of letters" :key="letter">{{ letter }}</router-link>
+      <router-link class="d-flex justify-center px-1" :to="{ name: 'ByLetter', params: { letter } }" v-for="letter in letters" :key="letter">{{ letter }}</router-link>
     </div>
+
+    <VDataTable
+      :headers="headers"
+      :items="filteredPratos"
+      :items-per-page="10"
+      class="elevation-1"
+    ></VDataTable>
   </VCard>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { VTextField } from "vuetify/components";
+import { computed, onMounted, ref, watch } from "vue";
+import { VCard, VTextField, VDataTable } from "vuetify/components";
 import store from "../src/store";
+import axiosClient from "../src/service/axiosCliente";
 
 const meals = computed(() => store.state.meals);
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const pratos = ref([]);
+const search = ref("");
+const filteredPratos = ref([]);
+
+const headers = [
+  { text: "ID", value: "idIngredient" },
+  { text: "Ingredient", value: "strIngredient" },
+  { text: "Description", value: "strDescription" },
+  { text: "Type", value: "strType" },
+];
+
+const filterMeals = () => {
+  const searchTerm = search.value.toLowerCase();
+  if (!searchTerm) {
+    filteredPratos.value = pratos.value;
+  } else {
+    filteredPratos.value = pratos.value.filter(prato =>
+      prato.idIngredient.toLowerCase().includes(searchTerm) ||
+      prato.strIngredient.toLowerCase().includes(searchTerm)
+    );
+  }
+};
+
+onMounted(async () => {
+  try {
+    const response = await axiosClient.get("/list.php?i=list");
+    if (response.data.meals) {
+      pratos.value = response.data.meals;
+      filterMeals(); 
+    } else {
+      pratos.value = [];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+
+watch(search, () => {
+  filterMeals();
+});
 </script>
--- views/MealList.vue
+++ views/MealList.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>
-        
-    </div>
-</template>
+    <VCard class="pa-5 d-flex justify-center flex-column" width="1200px" variant="outlined">
+      <h1>Meals starting with "{{ letter }}"</h1>
+      <VDataTable
+        :headers="headers"
+        :items="pratos"
+        :items-per-page="10"
+        class="elevation-1"
+      ></VDataTable>
+    </VCard>
+  </template>
+  
+  <script setup>
+    import { computed, onMounted, ref } from "vue";
+    import { VCard, VDataTable } from "vuetify/components";
+    import axiosClient from "../src/service/axiosCliente";
 
-<script setup></script>
+
+  </script>
+  
